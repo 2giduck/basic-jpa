5b0f74448ead2e42efc59e0ed4bf507d7e2f620a
Dulgiduck <jsh@sngy.io>
2022-04-11 09:30:49 +0900

    값 타입 매핑

--- src/main/java/basic/jpa/Member.java
+++ src/main/java/basic/jpa/Member.java
@@ -1,48 +1,14 @@
 package basic.jpa;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+
 
 @Entity
 public class Member {
     @Id @GeneratedValue
     private Long id;
+    private String name;
 
-    @Column(name = "USERNAME")
-    private String username;
-
-    @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
-    private Team team;
-
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void changeTeam(Team team) {
-        this.team = team;
-        this.team.getMembers().add(this);
-    }
+    @Embedded
+    private Address address;
 }
